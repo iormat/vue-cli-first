705b304b36cb8d7bba635fbd8f9a23dad13b4c86
iormat <iorio436@outlook.it>
2021-11-23 15:38:53 +0100

    first attempt using Vue CLI and components update

---
+++ src/components/Hero.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="hero-jumbo">
+        <img alt="Vue logo" src="../assets/logo.png">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    .hero-jumbo {
+        width: 90%;
+        margin: 100px auto;
+    }
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1> La mia prima app con Vue CLI! </h1>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+  props: {
+      myTitle : String,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1 {
+    text-align: center;
+    font-size: 40px;
+}
+</style>
